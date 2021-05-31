d4fe6dfe4becfd50da88ea78679d948e495baef2
Dami-Lapite <damilapite@gmail.com>
2021-05-31 13:41:49 -0600

    added flip animation and dynamic background color

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import { flipInX, pulse } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
+import ReactCardFlip from 'react-card-flip';
+import IconButton from '@material-ui/core/IconButton';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import './App.css';
-
-const styles = {
-    pulse: {
-      animation: 'x 5s',
-      animationName: Radium.keyframes(pulse, 'pulse')
-    },
-    flipInX: {
-      animation: 'x 5s',
-      animationName: Radium.keyframes(flipInX, 'flipInX')
-    }
-  }
 
 class App extends Component { 
 
@@ -23,7 +13,10 @@ class App extends Component {
       character: "",
       anime: "",
       hasQuote: false,
-      showQuoteText: true,
+      isFlipped: false,
+      colorIndex: 0,
+      backgroundColor: '#348AA7',
+      colorArray: ['#DB5A42','#FFD275','#A8DCD9','#E2A3C7','#B5CA8D','#DB6C79','#9BC1BC','#9C95DC'],
     }
   }
 
@@ -37,13 +30,24 @@ class App extends Component {
     }
   }
 
-  setQuote = (quoteData) => {
-    this.setState({quote: quoteData.quote, character: quoteData.character, anime: quoteData.anime, hasQuote: true});
+  getColor = () =>{
+    return this.state.colorArray[this.state.colorIndex];
+  }
+
+  setQuote = () =>{
+    let color = this.getColor();
+    let index = 0;
+    if (this.state.colorIndex < 7){
+      index = this.state.colorIndex + 1;
+    }
+    this.getQuote()
+    .then(data =>{
+      this.setState({quote: data.quote, character: data.character, anime: data.anime, hasQuote: true, backgroundColor: color, colorIndex: index, isFlipped: false});
+    });
   }
 
   toggleShow = ()=>{
-    let current = this.state.showQuoteText;
-    this.setState({showQuoteText: !current});
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
   }
 
   componentDidMount(){
@@ -54,18 +58,22 @@ class App extends Component {
   }
   render (){
     return (
-      <StyleRoot>
-        <div className="App">
-          <div className="container">
-            <div className="card" style={styles.pulse} onClick={this.toggleShow}>
-              {this.state.showQuoteText ? 
-              <p className="quoteText">"{this.state.quote}"</p>: 
-              <p className="quoteText">{this.state.character}<br></br>{this.state.anime}</p>}
-              <p>Click the card to see where this quote is from</p>
-            </div>
+      <div className="App">
+        <div className="container">
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+          <div className="card"  style={{...{backgroundColor: this.state.backgroundColor}}} onClick={this.toggleShow}>
+            <p className="quoteText">"{this.state.quote}"</p>
+            <small className="hint">Click the card to see who said this !</small>
+          </div>
+          <div className="card" style={{...{backgroundColor: this.state.backgroundColor}}} onClick={this.toggleShow}>
+            <p className="quoteText">{this.state.character}<br></br>{this.state.anime}</p>
+          </div>
+          </ReactCardFlip>
+          <div>
+            <IconButton onClick={this.setQuote} ><NavigateNextIcon style={{...{color: this.state.backgroundColor},fontSize:'200%'}}/></IconButton>
           </div>
         </div>
-      </StyleRoot>
+      </div>
     );
   }
 }
